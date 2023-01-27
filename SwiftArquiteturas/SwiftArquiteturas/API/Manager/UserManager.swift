3826cc48c108011ce58c10996e67815bd6ac440a
Felipe Angeli <felipe.angeli@gmail.com>
2023-01-27 11:31:05 -0300

    API
    construcao do Model, Provider, Manager, Bunsiness

---
+++ SwiftArquiteturas/SwiftArquiteturas/API/Manager/UserManager.swift
@@ -0,0 +1,50 @@
+//
+//  UserManager.swift
+//  SwiftArquiteturas
+//
+//  Created by Felipe  on 27/01/23.
+//
+
+import Foundation
+
+
+protocol UserManagerProtocol {
+    func register(email: String, password: String,
+                  sucessHandler: @escaping(UserModel) -> Void,
+                  failureHandler: @escaping(Error?) -> Void)
+    
+    func login(email: String, password: String, completionHandler: @escaping(Result<UserModel, Error>) ->Void)
+    
+}
+
+class UserManager: UserManagerProtocol{
+    let business: UserBusinessProtocol
+    
+    init(business: UserBusinessProtocol) {
+        self.business = business
+    }
+    
+    func register(email: String, password: String,
+                  sucessHandler: @escaping (UserModel) -> Void,
+                  failureHandler: @escaping (Error?) -> Void) {
+        business.register(email: email, password: password) { result in
+            switch result {
+                case .success(let userModel):
+                    sucessHandler(userModel)
+                case .failure(let error):
+                    failureHandler(error)
+            }
+        }
+    }
+    
+    func login(email: String, password: String, completionHandler: @escaping (Result<UserModel, Error>) -> Void) {
+        business.login(email: email, password: password) { result in
+            switch result {
+                case .success(let userModel):
+                    completionHandler(.success(userModel))
+                case .failure(let error):
+                    completionHandler(.failure(error))
+            }
+        }
+    }
+}
---
+++ SwiftArquiteturas/SwiftArquiteturas/infrastructure/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  SwiftArquiteturas
+//
+//  Created by Felipe  on 26/01/23.
+//
+
+import Foundation
+
+
+struct Constants {
+    struct ParametersKeys {
+        static let body = "body"
+        static let userModel = "userModel"
+    }
+}
